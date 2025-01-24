b2d7352f76a0e2ca662c6b82b55b0d59859fc1fe
Nicolas <berdunicolas@gmail.com>
2025-01-24 18:25:32 -0300

    Integration test apex-chart

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,12 @@
     <header class="p-5">
         <h5 class="display-5 header-section">¡Bienvenido, Nicolás Berdú!</h5>
     </header>
+    <div id="chart" class="card">
+        
+    </div>
 </main>
+
+<script type="module" src="{{ Vite::asset('resources/js/apex-chart.js') }}"></script>
+
 </x-app-layout>
 
