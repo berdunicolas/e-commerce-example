8b9dba4bd3e9f2eac29a304b6222e1cf45698f18
Nicolas <berdunicolas@gmail.com>
2025-02-11 14:52:28 -0300

    Fix navbar spelling

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,23 @@
 
 use App\Http\Controllers\Api\CategoryApiController;
 use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Middleware\EnsureCorrectAuthModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::name('api.')->group(function () {
-    Route::apiResource('categories', CategoryApiController::class)->middleware(['auth:sanctum'])->only(['index', 'store', 'show', 'update', 'destroy']);
-    Route::apiResource('products', ProductApiController::class)->middleware(['auth:sanctum'])->only(['index', 'store', 'show', 'update', 'destroy']);
+Route::middleware(['auth:sanctum', EnsureCorrectAuthModel::class.':web'])->name('api.')->group(function () {
+    Route::apiResource('categories', CategoryApiController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+    Route::apiResource('products', ProductApiController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+
+});
+
+
+Route::middleware(['auth:sanctum', EnsureCorrectAuthModel::class.':business'])->name('api.')->group(function () {
+    Route::get('catalog')->name('catalog');
+    
+    Route::get('businesses/businessinfo/', function (Request $request) {
+        return $request->user();
+    })->name('businessinfo');
+
 });
